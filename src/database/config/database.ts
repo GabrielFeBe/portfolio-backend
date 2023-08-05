a59f65997903070c084de68780cee915cfc3c077
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-08-05 15:56:28 -0300

    colocando porta

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,10 +2,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  password: process.env.DB_PASS || '0epjbTpMQvPc5MYOkGqG',
+  database: 'railway',
+  host: process.env.DB_HOST || 'containers-us-west-63.railway.app',
+  port: Number(process.env.DB_PORT) || 5808,
   dialect: 'mysql',
 };
 
