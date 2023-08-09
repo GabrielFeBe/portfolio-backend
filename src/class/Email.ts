f5038be0536a9a0136f7d676970558a71809722b
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-08-08 23:07:18 -0300

    validator

--- src/class/Email.ts
+++ src/class/Email.ts
@@ -1,8 +1,9 @@
+import validator from 'validator';
+
 export default class Email {
   private _email;
-  static emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/i;
   constructor(email: string) {
-    if (!Email.emailRegex.test(email)) {
+    if (!validator.isEmail(email)) {
       throw new Error('email invalido')
     }
     this._email = email;
