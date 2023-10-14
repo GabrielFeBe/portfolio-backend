28c1c19166ea0170cae4c8db1295de00c21b263c
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-10-14 19:29:35 -0300

    tirando dist

--- dist/src/interfaces/ICRUDModel.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=ICRUDModel.js.map
--- dist/src/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ErrorMiddleware {
-    static handler(err, req, res, _next) {
-        return res.status(500).json({ message: err.message });
-    }
-}
-exports.default = ErrorMiddleware;
-//# sourceMappingURL=errorMiddleware.js.map
