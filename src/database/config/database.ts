66c5071789b6eecbbc542a8ecc6b7e0cb1401225
Gabriel Fernandes <hu3master.zord@hotmail.com>
2024-01-17 23:13:38 -0300

    arrumando conexões

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,12 +1,12 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: 'railway',
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
-  dialect: 'mysql',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: 'postgres',
 };
 
 export = config;
